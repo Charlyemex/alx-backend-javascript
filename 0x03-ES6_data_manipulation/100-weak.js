abee8b89cadff5f03f14c8d53aae2d4a43ff9bb4
Charlyemex <charlyemex@gmail.com>
2024-01-09 13:03:25 +0000

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,28 +1,9 @@
 export const weakMap = new WeakMap();
 
-/**
- * The maximum number of calls for an endpoint.
- */
-const MAX_ENDPOINT_CALLS = 5;
-
-/**
- * Queries the API with the given endpoint and keeps track of the number
- * of times the endpoint has been called.
- *
- * @param {string} endpoint - The endpoint to query
- */
+// https://stackoverflow.com/questions/29413222/what-are-the-actual-uses-of-es6-weakmap
 export function queryAPI(endpoint) {
-  // If the endpoint has not been called before, set its value in the
-  // weakMap to 0
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-
-  // Increment the number of times the endpoint has been called
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-
-  // If the endpoint has been called too many times, throw an error
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-    throw new Error('Endpoint load is high');
-  }
+  let called = 0;
+  if (weakMap.get(endpoint)) called = weakMap.get(endpoint);
+  weakMap.set(endpoint, called + 1);
+  if (called + 1 >= 5) throw new Error('Endpoint load is high');
 }
